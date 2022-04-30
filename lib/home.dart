1c9e36282c73a9a21a8539be2811b374d00cff88
Marius Wichtner <2mawi2@gmail.com>
2022-04-30 09:10:06 +0200

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:my_qr_code_scanner/scanner.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: QRViewExample(),
+      ),
+    );
+  }
+}
